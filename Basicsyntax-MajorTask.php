bf8461150b11df54c8226a9c0a6150c983e77d77
Olu-Niyi <67210708+Olu-Niyi@users.noreply.github.com>
2020-10-15 20:22:43 +0100

    Add files via upload

---
+++ Basicsyntax-MajorTask.php
@@ -0,0 +1,22 @@
+<?php
+//====== First major task. ======
+
+/* 
+    Occupation: student
+    Gender: Female
+    Sidehustle Username: Makinde Iyanuoluwa
+*/
+
+//about me tag
+echo "<h1>About Me</h1> <hr/>";
+
+//side hustle username
+echo "<h3>Makinde Iyanuoluwa</h3>";
+echo "<p>I am the middle child in 3 children. 
+        Studying engineering because of my love for maths and drawings. 
+        I am involved in a lot of NGOs but I'm mostly in the media and photography department.
+</p>";
+
+
+
+?>
