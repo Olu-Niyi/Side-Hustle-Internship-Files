590f4611b8ae01a7a2ff3b2de5fbd91b706357ad
Olu-Niyi <67210708+Olu-Niyi@users.noreply.github.com>
2020-10-23 21:14:14 +0100

    Add files via upload

---
+++ Assignment.php
@@ -0,0 +1,22 @@
+<?php
+
+
+//array declaration
+    $Usernames = array("Ted", "Barney", "Marshall", "Lily", "Robin");
+  
+    if strlen($Usernames<6)
+  //function to generate 3 random numbers
+  function randomNumbers(){
+    for ($i=0; $i < 3; $i++) { 
+        $random = rand(100, 999);
+        return $random;
+    }    
+    
+    if (strlen($username >=8)
+    Echo "Username cannot be greater than 8 or less than 6"
+//headers for password declaration
+    $Passwords = array("abc", "def", "ghi", "jkl", "mno");
+    
+    echo "$Usernames $passwords";
+
+    ?> 
